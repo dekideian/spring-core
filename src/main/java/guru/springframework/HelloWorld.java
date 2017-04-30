2b3f8455262c28412811093ec0c0839f09df50e1
Deian <my email>
2017-04-30 21:48:19 +0300

    helloWorld spring

---
+++ src/main/java/guru/springframework/HelloWorld.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+	
+	public void sayHello(){
+		System.out.println("Hello nurse!");
+	}
+}
--- src/main/java/guru/springframework/SpringmvcApplication.java
+++ src/main/java/guru/springframework/SpringmvcApplication.java
@@ -2,11 +2,14 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringmvcApplication.class, args);
+    	ApplicationContext context =  SpringApplication.run(SpringmvcApplication.class, args);
+    	HelloWorld hello = (HelloWorld) context.getBean("helloWorld");
+    	hello.sayHello();
     }
 }
